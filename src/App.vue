10272ca223d013d8675a62ef9756bffc113678a5
CHEN LI MIN <ppy951@gmail.com>
2019-09-30 22:34:29 +0900

    canvas basic setting

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <night-sky />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import NightSky from './components/NightSky.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld,
+    NightSky,
   },
 };
 </script>
@@ -21,8 +20,9 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+}
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/NightSky.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="canvas-container">
+    <canvas ref="night-sky-canvas">Not support canvas</canvas>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'night-sky',
+  data() {
+    return {
+      canvasEl: null,
+    };
+  },
+  methods: {
+    setCanvas() {
+      const canvasEl = this.$refs['night-sky-canvas'];
+      this.ctx = canvasEl.getContext('2d');
+      canvasEl.width = canvasEl.parentElement.clientWidth;
+      canvasEl.height = canvasEl.parentElement.clientHeight;
+    },
+  },
+  mounted() {
+    this.setCanvas();
+  },
+};
+</script>
+
+<style scoped>
+.canvas-container {
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  background-image: linear-gradient(#070B34, #855988);
+}
+</style>
